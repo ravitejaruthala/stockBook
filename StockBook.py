01791a187697be774a02a090ed353fc2a7916175
Raviteja Ruthala <61064928+ravitejaruthala@users.noreply.github.com>
2022-08-15 22:23:25 -0400

    Update StockBook.py

--- StockBook.py
+++ StockBook.py
@@ -2,27 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-dataURL = "Data.csv"
-data = pd.read_csv(dataURL)
-df = pd.DataFrame(data)
 #stockSymbol transactionType	transactionDate	stockQuantity	stockPrice	stockInvestment	userComments
-st.set_page_config(layout="centered", page_title="StockBook")
-st.title("StockBook")
-st.write("This application helps you to track your investments.")
-st.write(df)
 
 #To add empty lines to the app.
 def space(num_lines=1):
     for _ in range(num_lines):
         st.write("")
-
-st.header("Portfolio Summary")
-space(3)
-st.header("Stock Summary")
-symbols = st.multiselect("Choose a particular stock to visualize", sorted(df["stockSymbol"].unique()))
-space(1)
-
-
 
 def stockInfo(selectedList):
 	for stock in selectedList:
@@ -36,40 +21,36 @@ def stockInfo(selectedList):
 		if holdingQty:
 			st.write("**Return till date:** *No return till date.*")
 		else:	
-			st.write("**Return till date:**")
+			st.write("**Return on investment:**")
 			st.metric(label='INR',
 					value=(sellStockDF['stockInvestment'].sum()- buyStockDF['stockInvestment'].sum()),
 					delta = ((sellStockDF['stockInvestment'].sum() - buyStockDF['stockInvestment'].sum())/buyStockDF['stockInvestment'].sum())*100)
 		space(2)
 
-def realized_gain_or_loss(df) :
-    years_list = df['transactionYear'].unique().tolist()
-    for year in years_list :
-        total_gain_or_loss = 0
-        df_to_consider = df[df['transactionYear']== year]
-        sell_transactions = df_to_consider[df_to_consider['transactionType'] == 'Sell']
-        if sell_transactions.empty :
-            st.write("**Realized Gain till date:** No Sell Transactions in ", year)
-        else :
-            stocks_list = sell_transactions['stockSymbol'].unique().tolist()
-            for stock in stocks_list :
-                filter_by_stock = df_to_consider[df_to_consider['stockSymbol']== stock]
-                buy_transactions = filter_by_stock[filter_by_stock['transactionType']== 'Buy']
-                Total_investment_in_year = buy_transactions['stockInvestment'].sum()
-                sell_transactions_in_year = sell_transactions[sell_transactions['stockSymbol'] == stock]
-                total_sell_in_year = sell_transactions_in_year['stockInvestment'].sum()
-                total_gain_or_loss += total_sell_in_year - Total_investment_in_year
-                st.write('Company Name : ', stock, '   |   Year : ' , year)
-                st.write("Gain/Loss : ")
-                st.metric(label='INR',
-					value=(total_sell_in_year - Total_investment_in_year),
-					delta = ((total_sell_in_year - Total_investment_in_year)/Total_investment_in_year)*100)
-                st.markdown("""<hr style="height:2px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
-        st.write("Year : ", year, "Total Returns : ", total_gain_or_loss )
-                     
+st.set_page_config(layout="centered", page_title="StockBook")
+st.title("StockBook")
+st.write("This application helps you to track your investments.")
+st.write('''We recommend you to use a specific format to track your portfolio. To start, you can download the sample file below. 
+			Or, if you already using one, please upload your file.''')
+st.download_button('Download your free sample', data="sample.csv", file_name='sample.csv')
+uploaded_file = st.file_uploader('Upload your file here!!', type=['csv'], accept_multiple_files=False)
+if uploaded_file is not None:
+	data = pd.read_csv(uploaded_file)
+	df = pd.DataFrame(data)
+	st.write(df)
+	st.header("Portfolio Summary")
+	portfolioSummary = px.pie(df, values='stockInvestment', names='stockSymbol')
+	st.write(portfolioSummary)
+	st.write('Need a pandas query that can retrive holding shares - Better to maintain a dict with share name and share quantity')
+	space(3)
+	st.header("Stock Summary")
+	symbols = st.multiselect("Choose a particular stock to retrive its summary", sorted(df["stockSymbol"].unique()))
+	space(1)
+	stockInfo(symbols)
+	st.header("Return statement")
+	st.write('A table that provides profit (descending) and loss (ascending')
+	st.header("Investment summary")
+	st.write('A statement that provides overview on investments')
 
-if __name__ == '__main__' :
-    stockInfo(symbols)
-    st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
-    st.header("Gain Summary")
-    realized_gain_or_loss(df)
+else:
+	st.write("You need to upload a file to track.")
